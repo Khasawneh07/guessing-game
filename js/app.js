a7e8c9fa39d662ecd4a32d8960f761713482ec8f
Malak Momani <malak.momani.96@gmail.com>
2021-02-09 17:27:02 +0200

    lab03 solved

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
 'strict use';
 
+
 let userName = prompt('What is your Name?');
 alert(`Welcome ${userName}, I hope to enjoy in my page`);
-
+let score = 0;
 
 let love = prompt('Do I love programming?');
 love = love.toLowerCase();
-if(love === 'yes' || love === 'y')
-{
+if (love === 'yes' || love === 'y') {
     //console.log('Correct');
     alert('Correct , I do love programming \n');
+    score++;
 }
-else if(love === 'no' || love === 'n') {
+else if (love === 'no' || love === 'n') {
     //console.log('Incorrect');
     alert('Incorrect , I do love programming \n');
 }
@@ -22,14 +23,14 @@ else {
 
 let from = prompt('Am I from Amman?');
 from = from.toLowerCase();
-if(from === 'yes' || from === 'y')
-{
+if (from === 'yes' || from === 'y') {
     //console.log('Incorrect');
     alert('Incorrect , I am from Irbid \n');
 }
-else if(from === 'no' || from === 'n') {
+else if (from === 'no' || from === 'n') {
     //console.log('Correct');
     alert('Correct , I am from Irbid \n');
+    score++;
 }
 else {
     alert('Try again');
@@ -38,12 +39,12 @@ else {
 
 let favColor = prompt('Is Orange my favourite color?');
 favColor = favColor.toUpperCase();
-if(favColor === 'YES' || favColor === 'Y')
-{
+if (favColor === 'YES' || favColor === 'Y') {
     //console.log(favColor +' Correct');
     alert('Correct , I like it \n');
+    score++;
 }
-else if(favColor === 'NO' || favColor === 'N') {
+else if (favColor === 'NO' || favColor === 'N') {
     //console.log('Incorrect');
     alert('Incorrect , Orange is my favourite color \n');
 }
@@ -55,14 +56,14 @@ else {
 
 let old = prompt('Am I 25 years old?');
 old = old.toLowerCase();
-if(old === 'yes' || old === 'y')
-{
+if (old === 'yes' || old === 'y') {
     //console.log('Incorrect');
     alert('Incorrect , I\'m 24 years old \n');
 }
-else if(old === 'no' || old === 'n') {
+else if (old === 'no' || old === 'n') {
     //console.log('Correct');
     alert('Correct , I\'m 24 years old \n');
+    score++;
 }
 else {
     alert('Try again');
@@ -70,17 +71,64 @@ else {
 
 let noChildren = prompt('Do I have duaghter?');
 noChildren = noChildren.toLowerCase();
-if(noChildren === 'yes' || noChildren === 'y')
-{
+if (noChildren === 'yes' || noChildren === 'y') {
     //console.log('Correct');
     alert('Yes her name is Lama \n');
+    score++;
 }
-else if(noChildren === 'no' || noChildren === 'n'){
-   //console.log('Incorrect');
-   alert('Incorrect , I have a daughter \n');
+else if (noChildren === 'no' || noChildren === 'n') {
+    //console.log('Incorrect');
+    alert('Incorrect , I have a daughter \n');
 }
 else {
-    alert('Try again');   
+    alert('Try again');
 }
 
-alert(`Thank you ${userName} , it was very easy , I wish to see you again`)
+let no_ = prompt('Guess! How many brothers and sisters does I have ?\n #### YOU HAVE 4 ATTEMPTS ####')
+
+let attempt = 0;
+do {
+    if (parseInt(no_) > 6) {
+        no_ = prompt('Too High ,Try Again');
+    }
+    else if (parseInt(no_) < 6) {
+        no_ = prompt('Too Low , Try again');
+    }
+    else if (parseInt(no_) == 6) {
+        alert('Good guessing, you Win');
+        score++
+        break;
+    }
+    attempt++;
+} while (attempt !== 3);
+
+console.log(no_);
+if (parseInt(no_) !== 6) {
+    alert(`Hard luck ${userName} , I have 6 brothers and sisters`);
+}
+
+
+let fov_lan = ['javascript', 'html', 'android','c++'];
+let userinput = prompt('What is my favourite Programming languages ? \n #### YOU HAVE 6 ATTMPTS ####');
+let flag = false;
+
+//console.log(fov_lan.length);
+
+for (let i = 0; i <= 5; i++) {
+
+    //console.log(userinput.toLowerCase());
+    for (let j = 0; j < fov_lan.length ; j++){
+        //console.log(userinput.toLowerCase() === fov_lan[j]);
+        if(userinput.toLowerCase() === fov_lan[j]) {
+            alert(`Good Job ${userName} , I like ${fov_lan[0]} , ${fov_lan[1]} , ${fov_lan[2]} and ${fov_lan[3]}`);
+            flag = true;
+            score++;
+            break;
+        }
+    }
+    if(flag){
+        break;
+    }
+    userinput = prompt('Try Again');
+}
+alert(`Thank you ${userName} \n your score is ${score} out of 7 , I wish to see you again`);
